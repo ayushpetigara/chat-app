5f9b55ab5bde0d4d74d71ce8c2f34194f2984e4f
Ayush Petigara <aarvyushpetigara3@gmail.com>
2018-10-28 18:29:18 -0400

    Add scroll when the user is at the bottom

--- public/js/index.js
+++ public/js/index.js
@@ -1,4 +1,20 @@
 var socket = io();
+//autoscrolling
+function scrollToBottom() {
+  // selectors
+  var messages = jQuery('#messages');
+  var newMessage = messages.children('li:last-child');
+  //height
+  var clientHeight = messages.prop('clientHeight');
+  var scrollTop = messages.prop('scrollTop');
+  var scrollHeight = messages.prop('scrollHeight');
+  var newMessageHeight = newMessage.innerHeight();
+  var lastMessageHeight = newMessage.prev().innerHeight();
+
+  if(clientHeight + scrollTop + lastMessageHeight >= scrollHeight){
+    messages.scrollTop(scrollHeight);
+  }
+}
 
 socket.on('connect', function () {
   console.log('Connected to server');
@@ -18,6 +34,7 @@ socket.on('newMessage', function (message) {
   });
 
   jQuery('#messages').append(html);
+  scrollToBottom();
 });
 
 socket.on('newLocationMessage', function (message) {
@@ -29,6 +46,7 @@ socket.on('newLocationMessage', function (message) {
     createdAt: formattedTime
   });
   jQuery('#messages').append(html);
+  scrollToBottom();
 });
 
 jQuery('#message-form').on('submit', function (e) {
